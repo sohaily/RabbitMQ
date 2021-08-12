2e2eb69ea72fffab7cff29c1f1d5e17942a44045
Sohail Shahzad <sohailshahzad932@gmail.com>
2021-08-12 16:00:53 +0500

    Net 5 RabbitMQ Producer and Consumer App
    Create Basic DotNet 5 app and user RabbitMQ Producer and Consumer in it

---
+++ RabbitMQ.Producer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672") // username: guest, password: guest
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            QueueConsumer.Consume(channel);
+        }
+    }
+}
---
+++ RabbitMQ.Producer/RabbitMQ.Producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Uri = new Uri("amqp://guest:guest@localhost:5672") // username: guest, password: guest
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            QueueProducer.Publish(channel);
+
+        }
+    }
+}
